8c68bb5b4a1ed4c4ae865d18f97485a8bc07a1f9
DH <daniel@vendettistudios.com>
2022-05-30 21:17:44 -0700

    Added background functionality, and enabled pulls from local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+// Dom elements
+// gets current day month and time in specific format
 let day = moment().format('dddd, MMMM Do YYYY');
 let nineAM = $("#9am");
 let tenAM = $("#10am");
@@ -10,5 +12,28 @@ let fourPM = $("#4pm")
 let fivePM = $("#5pm")
 let userInput;
 $('#currentDay').html(day);
-
+// gets current time
 let hoursNow = moment().hour();
+
+
+// gets saved information from local storage
+// sets background color based off time of day, past present & future colors
+function background(){
+    TimeBlocks.each(function() 
+    {
+        let hour = parseInt($(this).attr("data-hour").split('-')[0]);
+    
+        console.log(hour)
+    
+        if(hour < hoursNow) {
+            $(this).addClass("past");
+        }
+        else if(hour === hoursNow){
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("future")
+        }
+    
+    });}
+    
