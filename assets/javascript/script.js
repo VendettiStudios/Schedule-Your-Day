973ee455c129e165bdb5f11817440c46c9a436f3
DH <daniel@vendettistudios.com>
2022-05-30 21:20:38 -0700

    added reload functionality, displaying previous saves

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,6 +17,39 @@ let hoursNow = moment().hour();
 
 
 // gets saved information from local storage
+
+initPage()
+function initPage() {
+    console.log("hours" + " " + hoursNow);
+    let init9 = JSON.parse(localStorage.getItem("9 AM"));
+    nineAM.val(init9);
+    console.log("hours" + hoursNow)
+
+    let init10 = JSON.parse(localStorage.getItem("10 AM"));
+    tenAM.val(init10);
+
+    let init11 = JSON.parse(localStorage.getItem("11 AM"));
+    elevenAM.val(init11);
+    
+    let init12 = JSON.parse(localStorage.getItem("12 PM"));
+    twelveAM.val(init12);
+    
+    let init13 = JSON.parse(localStorage.getItem("1 PM"));
+    onePM.val(init13);
+    
+    let init14 = JSON.parse(localStorage.getItem("2 PM"));
+    twoPM.val(init14);
+    
+    let init15 = JSON.parse(localStorage.getItem("3 PM"));
+    threePM.val(init15);
+
+    let init16 = JSON.parse(localStorage.getItem("4 PM"));
+    fourPM.val(init16);
+
+    let init17 = JSON.parse(localStorage.getItem("5 PM"));
+    fivePM.val(init17);
+}
+
 // sets background color based off time of day, past present & future colors
 function background(){
     TimeBlocks.each(function() 
@@ -46,4 +79,4 @@ function background(){
         localStorage.setItem(hours, JSON.stringify(userInput));
     })
 
-    
+
